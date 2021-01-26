fbc8a656e67a81244bafce9344077d752f68ef85
Thi Vieira <thiiagoviieira@gmail.com>
2021-01-25 22:55:24 -0300

    0.30000000000000004

--- src/TestaConversao.java
+++ src/TestaConversao.java
@@ -4,5 +4,13 @@ public class TestaConversao {
         int valor = (int)salario;
         System.out.println(valor);
 
+//        long numeroGrande = 3213213213L;
+//        short valorPequeno = 2131;
+//        byte b = 127;
+
+        double valor1 = 0.2;
+        double valor2 = 0.1;
+        double total = valor1 + valor2;
+        System.out.println(total);
     }
 }
