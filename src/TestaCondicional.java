8b1cd5de42d39ff7e43eaa6c611eec168813b5f4
Thi Vieira <thiiagoviieira@gmail.com>
2021-01-26 16:46:58 -0300

    Módulo 6, if, else, else if

--- src/TestaCondicional.java
+++ src/TestaCondicional.java
@@ -2,8 +2,16 @@ public class TestaCondicional {
     public static void main(String[] args) {
         System.out.println("testando condicionais");
         int idade = 20;
+        int quantidadePessoas = 3;
+
         if (idade >= 18) {
             System.out.println("Você tem mais de 18 anos!");
+        } else {
+            if(quantidadePessoas >= 2){
+                System.out.println("Voce não tem 18, mas pode entrar porque está acompanhado.");
+            } else {
+                System.out.println("voce não pode entrar!");
+            }
         }
 
     }
